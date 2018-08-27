2bed7eadb28faedbd952b4495313c4fbccffe8b6
Darwin Smith II <dwin@dlsmi.com>
2018-08-27 15:04:28 -0400

    Fix error messages

--- jwt_credential_test.go
+++ jwt_credential_test.go
@@ -30,19 +30,19 @@ func TestCreateJWTCredential(t *testing.T) {
 	setupHandleFunc(t, mux, p, "POST", http.StatusCreated, respBody)
 	createResponse, createErr := c.CreateJWTCredential(consumerID, key, secret)
 	assert.NoError(t, createErr, "no error")
-	assert.NotNil(t, createResponse, "received response")
-	assert.NotNil(t, createResponse.ID, "received ID from response body")
-	assert.Equal(t, createResponse.ConsumerID, consumerID, "consumerID equal")
-	assert.Equal(t, createResponse.Key, key, "key equal")
-	assert.Equal(t, createResponse.Secret, secret, "secret equal")
+	assert.NotNil(t, createResponse, "did not receive response")
+	assert.NotNil(t, createResponse.ID, "did not receive ID from response body")
+	assert.Equal(t, consumerID, createResponse.ConsumerID, "consumerID not equal")
+	assert.Equal(t, key, createResponse.Key, "key not equal")
+	assert.Equal(t, secret, createResponse.Secret, "secret not equal")
 	// close server so we can create a new one for the next test
 	server.Close()
 
 	c, mux, server = setup()
 	setupHandleFunc(t, mux, p, "POST", http.StatusOK, respBody)
 	createResponse, createErr = c.CreateJWTCredential(consumerID, key, secret)
-	assert.Error(t, createErr, "has error")
-	assert.Nil(t, createResponse, "received no response")
+	assert.Error(t, createErr, "should return error")
+	assert.Nil(t, createResponse, "received response")
 }
 
 func TestDeleteJWTCredential(t *testing.T) {
@@ -56,12 +56,12 @@ func TestDeleteJWTCredential(t *testing.T) {
 
 	setupHandleFunc(t, mux, p, "DELETE", http.StatusNoContent, nil)
 	deleteErr := c.DeleteJWTCredential(consumerID, jwtID)
-	assert.NoError(t, deleteErr, "no error")
+	assert.NoError(t, deleteErr, "should not return error")
 	// close server so we can create a new one for the next test
 	server.Close()
 
 	c, mux, server = setup()
 	setupHandleFunc(t, mux, p, "DELETE", http.StatusOK, nil)
 	deleteErr = c.DeleteJWTCredential(consumerID, jwtID)
-	assert.Error(t, deleteErr, "has error")
+	assert.Error(t, deleteErr, "should return error")
 }
