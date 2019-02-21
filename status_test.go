587ad808091a00aa47d9fabddb161ffb4e9b0793
Kareem Hepburn <kareemgan@gmail.com>
2019-02-21 14:53:27 -0600

    Remove comment

--- status_test.go
+++ status_test.go
@@ -8,29 +8,6 @@ import (
 )
 
 func TestGetStatus(t *testing.T) {
-	// respBody := []byte(`
-	// {
-	// 	"server": {
-	// 		"total_requests": 3,
-	// 		"connections_active": 1,
-	// 		"connections_accepted": 1,
-	// 		"connections_handled": 1,
-	// 		"connections_reading": 0,
-	// 		"connections_writing": 1,
-	// 		"connections_waiting": 0
-	// 	},
-	// 	"database": {
-	// 		"reachable": true
-	// 	}
-	// }
-	// `)
-	// // Setup
-	// c, mux, server := setup()
-	// defer teardown(server)
-	// p := path.Join("/status")
-	// setupHandleFunc(t, mux, p, "GET", http.StatusOK, respBody)
-	// /
-	// Send Status Request
 	u, err := url.Parse(kongURL())
 	require.NoError(t, err)
 	client := NewClient(1, 1, u)
